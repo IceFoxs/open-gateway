6abba436173fa3f8c988e4bb2d7ead2e91434517
icefox <yuntao_zhao@163.com>
2024-09-19 13:03:52 +0800

    x

---
+++ rpc/uni_invoker.go
@@ -0,0 +1,40 @@
+package rpc
+
+import (
+	"context"
+	"errors"
+	"github.com/IceFoxs/open-gateway/cache/gatewaymethod"
+	"github.com/IceFoxs/open-gateway/client"
+	"github.com/IceFoxs/open-gateway/common"
+	"github.com/IceFoxs/open-gateway/constant"
+	ge "github.com/IceFoxs/open-gateway/rpc/generic"
+	"github.com/IceFoxs/open-gateway/util"
+	"github.com/cloudwego/hertz/pkg/common/hlog"
+	"time"
+)
+
+func Invoke(ctx context.Context, filename string, param interface{}) (interface{}, error) {
+	hlog.Infof("Invoke filename:[%s]", filename)
+	gmm, ok := gatewaymethod.GetGatewayMethodCache().GetCache(filename)
+	if !ok {
+		return nil, errors.New("未找到服务调用信息")
+	}
+	hlog.Infof("found rpc invoke metadata --------------- %s", gmm)
+	if gmm.RpcType == constant.RPC_DUBOO {
+		re := ge.NewRefConf1(gmm.InterfaceName, "nacos", "interface", "dubbo", "127.0.0.1:8848", "nacos", "nacos")
+		time.Sleep(1 * time.Second)
+		toMap, err := util.JsonStringToMap(param.(string))
+		if err != nil {
+			return nil, err
+		}
+		hlog.Infof("toMap %s", common.ToJSON(toMap))
+		util.ConvertHessianMap(toMap)
+		data, err := ge.Invoke(re, gmm.MethodName, gmm.ParameterTypeName, util.ConvertHessianMap(toMap))
+		return data, err
+	}
+	if gmm.RpcType == constant.RPC_HTTP {
+		data, err := client.GetHttpClient().Post(ctx, gmm.AppName, gmm.Path, param)
+		return data, err
+	}
+	return nil, nil
+}
